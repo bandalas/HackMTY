249e4903f65a097f07bd5b5b9a8a20ed8980690d
bandala <k.rbandala@gmail.com>
2017-08-27 00:21:24 -0500

    First Commit

---
+++ Ejemplo.js
@@ -0,0 +1,50 @@
+// Example 1: sets up service wrapper, sends initial message, and
+// receives response.
+var prompt = require('prompt-sync')();
+var ConversationV1 = require('watson-developer-cloud/conversation/v1');
+
+// Set up Conversation service wrapper.
+var conversation = new ConversationV1({
+  username: 'a24e3ce5-b110-4bd7-aa50-4f96be7e6584',
+  password: 'hlGH2th0xv27',
+  path: { workspace_id: '19a57598-bb5c-45ee-8999-bdb10f0232a0' }, // replace with workspace ID
+  version_date: '2016-07-11'
+});
+
+// Start conversation with empty message.
+conversation.message({}, processResponse);
+
+// Process the conversation response.
+function processResponse(err, response) {
+  if (err) {
+    console.error(err); // something went wrong
+    return;
+  }
+
+  var endConversation = false;
+
+  // Check for action flags.
+  if (response.output.action === 'display_time') {
+    // User asked what time it is, so we output the local system time.
+    console.log('The current time is ' + new Date().toLocaleTimeString());
+  } else if (response.output.action === 'end_conversation') {
+    // User said goodbye, so we're done.
+    console.log(response.output.text[0]);
+    endConversation = true;
+  } else {
+    // Display the output from dialog, if any.
+    if (response.output.text.length != 0) {
+        console.log(response.output.text[0]);
+    }
+  }
+
+  // If we're not done, prompt for the next round of input.
+  if (!endConversation) {
+    var newMessageFromUser = prompt('>> ');
+    conversation.message({
+      input: { text: newMessageFromUser },
+      // Send back the context to maintain state.
+      context : response.context,
+    }, processResponse)
+  }
+}
